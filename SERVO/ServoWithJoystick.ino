4ec78b8fffc946a9a68c4a4d3fe73371698d76a6
futurecrafts <biny38317@naver.com>
2018-07-26 05:32:02 +0000

    Update ServoWithJoystick.ino

--- SERVO/ServoWithJoystick.ino
+++ SERVO/ServoWithJoystick.ino
@@ -1,17 +1,18 @@
 #include <Servo.h>
 
+int ServoPin1 = 3;
+int ServoPin2 = 5;
+int joyX = 0;
+int joyY = 1;
+int joyVal;
+
 Servo servo1;
 Servo servo2;
 
-int joyX = 0;
-int joyY = 1;
-
-int joyVal;
-
 void setup() {
   // put your setup code here, to run once:
-  servo1.attach(3);
-  servo2.attach(5);
+  servo1.attach(ServoPin1);
+  servo2.attach(ServoPin2);
 }
 
 void loop() {
@@ -19,9 +20,10 @@ void loop() {
   joyVal = analogRead(joyX);
   joyVal = map(joyVal, 0, 1023, 0, 180);
   servo1.write(joyVal);
-
+  
   joyVal = analogRead(joyY);
   joyVal = map(joyVal, 0, 1023, 0, 180);
   servo2.write(joyVal);
   delay(15);
+}
 }
